7f0b82caad05d02074f55788e68f193499262a1d
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-27 22:31:31 -0700

    reframed input and animation strings into const variables

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -11,8 +11,9 @@ public partial class Player : CharacterBody3D
 
     public override void _Ready()
     {
-        animPlayerNode.Play("Idle");
+        animPlayerNode.Play(GameConstants.ANIM_IDLE);
     }
+
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
@@ -24,16 +25,16 @@ public partial class Player : CharacterBody3D
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(
-            "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+            GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD
         );
 
         if (direction == Vector2.Zero)
         {
-            animPlayerNode.Play("Idle");
+            animPlayerNode.Play(GameConstants.ANIM_IDLE);
         }
         else
         {
-            animPlayerNode.Play("Move");
+            animPlayerNode.Play(GameConstants.ANIM_MOVE);
         }
     }
 }
